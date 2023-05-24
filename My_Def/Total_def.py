9f21de9d58a898c3ab962172d9851079356109e9
jw <malinup@naver.com>
2023-05-24 09:39:14 +0900

    commit

---
+++ My_Def/Total_def.py
@@ -0,0 +1,212 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import scipy.stats # 상관관계 분석 import
+from datetime import date, timedelta
+
+def Read_data(filename):
+    df = pd.read_csv("C:/Users/jw/Desktop/TeamProject_Flask_Prac/{}.csv".format(filename))
+#     df.set_index('Date', inplace=True)
+#     df.sort_index(ascending=True, inplace=True)
+    df['Name'] = filename # 식별자 추가(나중에 분석 돌리기 위해서 추가함..)
+    return df
+
+
+def Make_Data_Polynomial(filename):
+    df = Read_data(filename)
+    X = df['Date'].values # 처음부터 마지막 컬럼 직전까지의 데이터 (독립 변수 - 원인)
+    y = df['Close'].values # 마지막 컬럼 데이터 (종속 변수 - 결과)
+    df = pd.DataFrame(X, columns=['Date'])
+    # 'Date' 열의 데이터를 날짜로 변환
+    df['Date'] = pd.to_datetime(df['Date'])
+    # 'Date' 열의 데이터를 숫자로 변환
+    df['Date_numeric'] = pd.factorize(df['Date'])[0]
+    X = df['Date'].values 
+    X = df['Date_numeric'].values
+    X = X.reshape(-1, 1)
+    return X, y
+
+
+
+def Polynomial_graph(filename, degree):
+    X = Make_Data_Polynomial(filename)[0]
+    y = Make_Data_Polynomial(filename)[1]
+    poly_reg = PolynomialFeatures(degree=degree)
+    X_poly = poly_reg.fit_transform(X)
+    lin_reg = LinearRegression()
+    lin_reg.fit(X_poly, y)
+
+    x_range = np.linspace(X.min(), X.max(), 100)
+    X_range_poly = poly_reg.transform(x_range.reshape(-1, 1))
+    y_range = lin_reg.predict(X_range_poly)
+
+    fig = px.scatter(x=X.flatten(), y=y, opacity=0.65)
+    fig.add_traces(go.Scatter(x=x_range,
+                            y=y_range
+                            , name='Polynomial Fit')
+                            )
+    fig.add_trace(go.Scatter(
+    x=X.flatten(),
+    y=y,
+    mode='markers',
+    name='Value',
+    marker=dict(
+        color='blue',opacity=0.5  # 원하는 색상으로 변경
+    )
+))
+
+    fig.update_layout(
+        title='Polynomial Model {}'.format(filename),
+        xaxis_title='date',
+        yaxis_title='value',
+        autosize=True
+    )
+    return fig
+
+
+def Polynomial_Predict_days(filename, degree, end):# degree 는 숫자 , end일 후 까지 예측
+    X = Make_Data_Polynomial(filename)[0]
+    y = Make_Data_Polynomial(filename)[1]
+    result = []
+    poly_reg = PolynomialFeatures(degree=degree) # 그래프의 X 계수를 결정
+    X_poly = poly_reg.fit_transform(X)
+    lin_reg = LinearRegression()
+    lin_reg.fit(X_poly, y) # 변환된 X 와 y 를 가지고 모델 생성 (학습)
+    for i in range(int(Make_Data_Polynomial(filename)[0][-1]) + 1, int(Make_Data_Polynomial(filename)[0][-1]) + 1 + end):
+        predict_value = lin_reg.predict(poly_reg.fit_transform([[i]]))
+        result.append(predict_value)
+    return result
+
+
+def Change_DataFrame(filename, degree, end):
+    df = pd.DataFrame(Polynomial_Predict_days(filename, degree, end), columns=['Close'])
+    df['Name'] = filename
+    return df
+
+
+def anal_scipy(*dfs):
+    result = []
+    for i in range(len(dfs)):
+        for j in range(i+1, len(dfs)):
+            x = dfs[i]
+            y = dfs[j]
+            result_key = str(x['Name'].iloc[0]) + ", " + str(y['Name'].iloc[0]) # UK,KS200 이런 형태로 만들어줌
+            result_value = (result_key, scipy.stats.pearsonr(x['Close'], y['Close']))
+            result.append(result_value)
+
+    return result
+
+
+def Change_DataFrame(filename, degree, end):
+    df = pd.DataFrame(Polynomial_Predict_days(filename, degree, end), columns=['Close'])
+    df['Name'] = filename
+    return df
+
+def spicy_graph(*dfs):
+    My_data = anal_scipy(*dfs)
+    result_index = []
+    result_value = []
+    for x in range(len(My_data)):
+        result_index.append(My_data[x][0])
+        result_value.append(My_data[x][1][0])
+        
+    table_data = [['', 'Value']]
+    teams = []
+    Value = []
+
+    for i in range(len(result_index)):
+        table_data.append([result_index[i], result_value[i]])
+        teams.append(result_index[i])
+        Value.append(result_value[i])
+
+    fig = ff.create_table(table_data, height_constant=60)
+
+    # Make traces for graph
+    max_abs_value_index = max(range(len(Value)), key=lambda i: abs(Value[i]))
+    colors = ['#0099ff' if idx != max_abs_value_index else '#ff0000' for idx in range(len(Value))]
+
+    trace1 = go.Bar(x=teams, y=Value, xaxis='x2', yaxis='y2',
+                    marker=dict(color=colors),
+                    name='Value')
+
+    # Add trace data to figure
+    fig.add_traces([trace1])
+
+    # initialize xaxis2 and yaxis2
+    fig['layout']['xaxis2'] = {}
+    fig['layout']['yaxis2'] = {}
+
+    # Edit layout for subplots
+    fig.layout.yaxis.update({'domain': [0, .45]})
+    fig.layout.yaxis2.update({'domain': [.6, 1]})
+
+    # The graph's yaxis2 MUST BE anchored to the graph's xaxis2 and vice versa
+    fig.layout.yaxis2.update({'anchor': 'x2'})
+    fig.layout.xaxis2.update({'anchor': 'y2'})
+    fig.layout.yaxis2.update({'title': 'Value'})
+
+    # Update the margins to add a title and see graph x-labels.
+    fig.layout.margin.update({'t':75, 'l':50})
+    fig.layout.update({'title': 'scipy Analysis'})
+
+    # Update the height because adding a graph vertically will interact with
+    # the plot height calculated for the table
+    fig.layout.update({'height':800})
+
+    # Plot!
+    return fig
+
+
+def spicy_graph_avg(index_values, value_values):
+    table_data = [['', 'Value']]
+    teams = []
+    Value = []
+
+    for i in range(len(index_values)):
+        table_data.append([index_values[i], value_values[i]])
+        teams.append(index_values[i])
+        Value.append(value_values[i])
+
+    fig = ff.create_table(table_data, height_constant=60)
+
+    # Make traces for graph
+    max_abs_value_index = max(range(len(Value)), key=lambda i: abs(Value[i]))
+    colors = ['#0099ff' if idx != max_abs_value_index else '#ff0000' for idx in range(len(Value))]
+
+    trace1 = go.Bar(x=teams, y=Value, xaxis='x2', yaxis='y2',
+                    marker=dict(color=colors),
+                    name='Value')
+
+    # Add trace data to figure
+    fig.add_traces([trace1])
+
+    # initialize xaxis2 and yaxis2
+    fig['layout']['xaxis2'] = {}
+    fig['layout']['yaxis2'] = {}
+
+    # Edit layout for subplots
+    fig.layout.yaxis.update({'domain': [0, .45]})
+    fig.layout.yaxis2.update({'domain': [.6, 1]})
+
+    # The graph's yaxis2 MUST BE anchored to the graph's xaxis2 and vice versa
+    fig.layout.yaxis2.update({'anchor': 'x2'})
+    fig.layout.xaxis2.update({'anchor': 'y2'})
+    fig.layout.yaxis2.update({'title': 'Value'})
+
+    # Update the margins to add a title and see graph x-labels.
+    fig.layout.margin.update({'t':75, 'l':50})
+    fig.layout.update({'title': 'scipy Analysis'})
+
+    # Update the height because adding a graph vertically will interact with
+    # the plot height calculated for the table
+    fig.layout.update({'height':800})
+
+    # Plot!
+    return fig
+
+
+
--- main.py
+++ main.py
@@ -4,68 +4,107 @@ import pandas as pd
 import plotly.express as px
 from flask import Flask, render_template, request, redirect
 from flask import url_for
-from random import *
 import plotly.graph_objects as go
-from My_Def.lotto import My_lotto
-from My_Def.showgraph import displaygraph
+from My_Def.Total_def import *
+
+
+
+UK_df = Read_data("UK")
+KS200_df = Read_data("KS200")
+OIL_df = Read_data("OIL")
+CD91_df = Read_data("CD91")
+SamSung_df = Read_data("SamSung")
+
+
+UK_data_df = Change_DataFrame("UK", 5, 5)
+KS200_data_df = Change_DataFrame("KS200", 4, 5)
+OIL_data_df = Change_DataFrame("OIL", 7, 5)
+CD91_data_df = Change_DataFrame("CD91", 5, 5)
+SamSung_data_df = Change_DataFrame("SamSung", 4, 5)
+
+
+Statistic_avg = []
+Statistic_index = []
+Data1 = anal_scipy(UK_data_df, KS200_data_df, OIL_data_df, CD91_data_df)
+Data2 = anal_scipy(UK_df, KS200_df, OIL_df, CD91_df)
+
+for x in range(len(anal_scipy(UK_df, KS200_df, OIL_df, CD91_df))):
+    Statistic_avg.append((Data1[x][1][0] * (95/100)) + (Data2[x][1][0] * (5/100)))
+    Statistic_index.append(Data1[x][0])
+
+
+Standard_index = UK_df, KS200_df, CD91_df
+Standard_predict_index = UK_data_df, KS200_data_df, CD91_data_df
+
+
+Standard_result = []
+for x in Standard_index:
+    Standard_result.append(anal_scipy(SamSung_df, x))
+
+
+Standard_result = [item for sublist in Standard_result for item in sublist]
+
+
+
+Standard_predict_result = []
+for x in Standard_predict_index:
+    Standard_predict_result.append(anal_scipy(SamSung_data_df, x))
+
+
+Standard_predict_result = [item for sublist in Standard_predict_result for item in sublist]
+
+
+
+SamSung_Graph_index = []
+SamSung_Graph_Value1 = []
+SamSung_Graph_Value2 = []
+for x in range(len(Standard_result)):
+    SamSung_Graph_index.append(Standard_result[x][0])
+    SamSung_Graph_Value1.append(Standard_result[x][1][0])
+    SamSung_Graph_Value2.append(Standard_predict_result[x][1][0])
+
+
+SamSung_Graph_avg = []
+for x in range(len(SamSung_Graph_Value1)):
+    SamSung_Graph_avg.append(
+       (SamSung_Graph_Value1[x] * (95 / 100)) +  SamSung_Graph_Value2[x] * (5 / 100))
+
+
 
 app = Flask("Project")
 
 
-# @app.route("/")
-# def home():
-#   return render_template("home.html")
 
 @app.route("/")
 def SPS_main():
-  graph_html_1 = displaygraph().to_html(full_html=False)
-  graph_html_2 = displaygraph().to_html(full_html=False)
-  graph_html_3 = displaygraph().to_html(full_html=False)
-  graph_html_4 = displaygraph().to_html(full_html=False)
-  return render_template("SPS_main.html", graph_html_1=graph_html_1, 
+  graph_html_1 = Polynomial_graph("SamSung",4).to_html(full_html=False)
+  graph_html_2 = Polynomial_graph("UK",5).to_html(full_html=False)
+  graph_html_3 = Polynomial_graph("KS200",4).to_html(full_html=False)
+  graph_html_4 = Polynomial_graph("OIL",7).to_html(full_html=False)
+  graph_html_5 = Polynomial_graph("CD91",5).to_html(full_html=False)
+  url1 = url_for('SPS_main02')
+  return render_template("SPS_main01.html", graph_html_1=graph_html_1, 
                          graph_html_2=graph_html_2, graph_html_3=graph_html_3,
-                         graph_html_4=graph_html_4)
+                         graph_html_4=graph_html_4, graph_html_5=graph_html_5,
+                         url1=url1)
 
 
-@app.route("/data1")
-def data1():
-  total = [x for x in range(1, 46)]
-  result = []
-  shuffle(total)
-  for x in range(0, 6):
-    x = total.pop()
-    result.append(x)
+@app.route("/SPS_main02")
+def SPS_main02():
+  graph_html_1 = spicy_graph(UK_df, KS200_df, OIL_df, CD91_df).to_html(full_html=False)
+  graph_html_2 = spicy_graph(UK_data_df, KS200_data_df, OIL_data_df, CD91_data_df
+                             ).to_html(full_html=False)
+  graph_html_3 = spicy_graph_avg(Statistic_index, Statistic_avg).to_html(full_html=False)
+  url1 = url_for("SPS_main03")
 
-  return render_template("data1.html", numbers=result)
+  return render_template("SPS_main02.html", graph_html_1=graph_html_1, graph_html_2=graph_html_2,
+                         graph_html_3=graph_html_3, url1=url1)
 
-
-@app.route("/data2")
-def data2():
-  total = [x for x in range(1, 46)]
-  result = []
-  shuffle(total)
-  for x in range(0, 6):
-    x = total.pop()
-    result.append(x)
-
-  return render_template("data2.html", numbers=result)
-
-@app.route("/image")
-def image():
-  return render_template("image.html")
-
-
-@app.route("/Prac")
-def Prac():
-  url1 = url_for('home')
-  url2 = url_for('data1')
-  url3 = url_for('data2')
-  return render_template("Prac.html", url1=url1, url2=url2, url3=url3)
-
-@app.route("/graph")
-def graph():
-  graph_html = displaygraph().to_html(full_html=False)
-  return render_template("graph.html", graph_html=graph_html)
-
+@app.route("/SPS_main03")
+def SPS_main03():
+   graph_html_1 = spicy_graph_avg(SamSung_Graph_index, SamSung_Graph_avg).to_html(
+      full_html=False)
+   
+   return render_template("SPS_main03.html", graph_html_1=graph_html_1)
 
 app.run("0.0.0.0")
