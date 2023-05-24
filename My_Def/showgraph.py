d8c685a9f55aee586b9fbd73e4343ffd8cee5f65
jw <malinup@naver.com>
2023-05-24 13:17:22 +0900

    commit

--- My_Def/showgraph.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import plotly.express as px
-
-
-def displaygraph():
-    df = px.data.gapminder().query("country=='Canada'")
-    fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
-    
-    return fig
